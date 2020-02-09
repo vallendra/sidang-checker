23327439e899db196d097834c9ae484c80a8749c
vallendra <kyky.farly@ralali.com>
2020-02-09 13:04:43 +0700

    fix

--- index.js
+++ index.js
@@ -20,11 +20,22 @@ const app = express();
 
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
+app.get('/callback', (req, res) => res.end(`I'm listening. Please access with POST.`));
+
+// webhook callback
 app.post('/callback', line.middleware(config), (req, res) => {
-  console.log(req);
-  Promise
-    .all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result))
+  if (req.body.destination) {
+    console.log("Destination User ID: " + req.body.destination);
+  }
+
+  // req.body.events should be an array of events
+  if (!Array.isArray(req.body.events)) {
+    return res.status(200).end();
+  }
+
+  // handle events separately
+  Promise.all(req.body.events.map(handleEvent))
+    .then(() => res.end())
     .catch((err) => {
       console.error(err);
       res.status(200).end();
