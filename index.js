a60048900c4bf49b0a7246f5c59fb50c2ce3d548
vallendra <kyky.farly@ralali.com>
2020-02-09 10:49:31 +0700

    200

--- index.js
+++ index.js
@@ -25,6 +25,10 @@ app.post('/callback', line.middleware(config), (req, res) => {
   Promise
     .all(req.body.events.map(handleEvent))
     .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(200).end();
+    });
 });
 
 app.use((err, req, res, next) => {
